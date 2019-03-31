123e04beae3863cb5837bfc4e20fe4c4bfd08f98
Steffen Weisshaar <steffenweisshaar@yahoo.de>
2019-03-31 13:57:57 +0200

    Fixed small bugs

--- contact-form.php
+++ contact-form.php
@@ -1,16 +1,17 @@
 <?php
-if(isset($_POST['submit'])) {
-    $vorname = $_POST['vorname'];
-    $nachname = $_POST['nachname'];
-    $emailFrom = $_POST['email'];
-    $subject = $_POST['betreff'];
-    $message = $_POST['text'];
-    if(!empty($vorname) && !empty($nachname) && !empty($emailFrom) && !empty($subject) && !empty($message)) {
-      $mailTo = "kontakt@agileDev-koeln.de";
-      $headers = "From: ".$emailFrom;
-      $text = "You have received an e-mail from ".$vorname." ".$nachname.".\n\n".$message;
-      mail($mailTo, $subject, $text, $headers);
-    }
-}
-header("Location:https://agileDev-koeln.de/index.html#Formular-message-send");
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $emailFrom = $_POST['email'];
+  $subject = $_POST['betreff'];
+  $message = $_POST['text'];
+  if(!empty($name) && !empty($emailFrom) && !empty($subject)) {
+    $mailTo = "kontakt@agileDev-koeln.de";
+    $headers = "From: ".$emailFrom;
+    $text = "You have received an e-mail from ".$name.".\n\n".$message;
+    mail($mailTo, $subject, $text, $headers);
+    header("Location:https://agileDev-koeln.de/index.html#Formular-message-send");
+    echo "After Header";
+  }} else{
+    echo "Hier ist etwas schief gelaufen.";
+  }
 ?>
